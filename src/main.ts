2be065f3845b1a5e54afb9f3101deb784d8c358a
Bia Brauna <biabn443@email.com>
2025-05-06 13:22:42 -0300

    fix: cors local host

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://econsciente-app.netlify.app', 'https://localhost:5173', 'http://localhost:3000'],
+    origin: ['https://econsciente-app.netlify.app', 'http://localhost:5173', 'http://localhost:3000'],
     methods: 'GET, POST, PUT, DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
