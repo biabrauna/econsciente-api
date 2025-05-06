97993c0091da3ed02796ac2f315cc2c1fcb2bfa6
Bia Brauna <biabn443@email.com>
2025-05-06 13:27:13 -0300

    fix: cors local host

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,23 +7,10 @@ import { DesafiosModule } from './desafios/desafios.module';
 import { ProfilePicModule } from './profile-pic/profile-pic.module';
 import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
-import { FallbackController } from './fallback.controller';
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client', 'dist'),
-    }),
-    AuthModule,
-    PrismaModule,
-    UsersModule,
-    PostsModule,
-    DesafiosModule,
-    ProfilePicModule,
-  ],
-  controllers: [AppController, FallbackController],
+  imports: [AuthModule, PrismaModule, UsersModule, PostsModule, DesafiosModule, ProfilePicModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
